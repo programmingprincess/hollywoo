682205f5edefaacdce07a429f5c8fa177424c7eb
programmingprincess <jiaqiwu@asu.edu>
2016-02-06 14:19:47 -0700

    final-commit
    implemented image options

--- hollywoo/MoviesViewController.swift
+++ hollywoo/MoviesViewController.swift
@@ -31,12 +31,15 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.insertSubview(refreshControl, atIndex: 0)
         
         networkRequest()
+
+        
     }
     
     func networkRequest() {
         // Do any additional setup after loading the view.
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
+        
         //endpoint string allows user to decide whether they want top rated movies or all movies to be displayd in tab navigation
         
         let request = NSURLRequest(
@@ -50,8 +53,16 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             delegateQueue: NSOperationQueue.mainQueue()
         )
         
+        // Display HUD right before the request is made
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        
+        
         let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
+                
+                //Hide HUD once network request comes back
+                MBProgressHUD.hideHUDForView(self.view, animated: true)
+                
                 if let data = dataOrNil {
                     //data is coming back as responseDictionary
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
@@ -64,6 +75,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                     }
                 }
         })
+
+        
         task.resume()
         
         
@@ -94,69 +107,89 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         //if not nil, execute!
         if let posterPath = movie["poster_path"] as? String {
-        
             let baseUrl = "http://image.tmdb.org/t/p/w500"
             let imageUrl = NSURL(string: baseUrl + posterPath)
-            cell.posterView.setImageWithURL(imageUrl!)
+            //cell.posterView.setImageWithURL(imageUrl!)
             
+            
+            let lowResUrl = "https://image.tmdb.org/t/p/w45" + posterPath
+            let highResUrl = "https://image.tmdb.org/t/p/original" + posterPath
+            
+            let smallImageRequest = NSURLRequest(URL: NSURL(string: lowResUrl)!)
+            let largeImageRequest = NSURLRequest(URL: NSURL(string: highResUrl)!)
+            
+            cell.posterView.setImageWithURLRequest(
+                smallImageRequest,
+                placeholderImage: nil,
+                success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
+                    
+                    // smallImageResponse will be nil if the smallImage is already available
+                    // in cache (might want to do something smarter in that case).
+                    cell.posterView.alpha = 0.0
+                    cell.posterView.image = smallImage;
+                    
+                    UIView.animateWithDuration(0.3, animations: { () -> Void in
+                        
+                        cell.posterView.alpha = 1.0
+                        
+                        }, completion: { (sucess) -> Void in
+                            
+                            // The AFNetworking ImageView Category only allows one request to be sent at a time
+                            // per ImageView. This code must be in the completion block.
+                           cell.posterView.setImageWithURLRequest(
+                                largeImageRequest,
+                                placeholderImage: smallImage,
+                                success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                                    
+                                    cell.posterView.image = largeImage;
+                                    
+                                },
+                                failure: { (request, response, error) -> Void in
+                                    // do something for the failure condition of the large image request
+                                    // possibly setting the ImageView's image to a default image
+                            })
+                    })
+                },
+                failure: { (request, response, error) -> Void in
+                    // do something for the failure condition
+                    // possibly try to get the large image
+            })
+            
+            //fade in image loaded from network
+             let imageRequest = NSURLRequest(URL: imageUrl!)
+            
+             cell.posterView.setImageWithURLRequest(
+             imageRequest,
+             placeholderImage: nil,
+             success: { (imageRequest, imageResponse, image) -> Void in
+             
+             // imageResponse will be nil if the image is cached
+             if imageResponse != nil {
+                print("Image was NOT cached, fade in image")
+                cell.posterView.alpha = 0.0
+                cell.posterView.image = image
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    cell.posterView.alpha = 1.0
+             })}
+             else {
+                print("Image was cached so just update the image")
+                cell.posterView.image = image
+                }},
+             failure: { (imageRequest, imageResponse, error) -> Void in
+                print("Error: failure clause executed")
+             })
         }
+        //set selected tableview cell background color
+        let backgroundView = UIView()
         
-        //use cocoapods here to import image into image view! because a func doesn't exist :D 
+        backgroundView.backgroundColor = UIColor(red: 0/255, green: 230/255, blue: 234/255, alpha: 1.0)
+        cell.selectedBackgroundView = backgroundView
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-
-        //! force cast = i'm positive that movies has something that's not nill!! if it is nil = app will crash
-        
-        //cell.textLabel!.text = title   
-        //the above is built in text label.
-        
-        //print("row \(indexPath.row)", terminator: "")
         
         return cell 
-    }
-    
-    func loadDataFromNetwork() {
-        
-        // ... Create the NSURLRequest (myRequest) ...
-        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
-        let myRequest = NSURLRequest(
-            URL: url!,
-            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
-            timeoutInterval: 10)
-        
-        // Configure session so that completion handler is executed on main UI thread
-        let session = NSURLSession(
-            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-            delegate:nil,
-            delegateQueue:NSOperationQueue.mainQueue()
-        )
-        
-        // Display HUD right before the request is made
-        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-        
-        let task : NSURLSessionDataTask = session.dataTaskWithRequest(myRequest,
-            completionHandler: { (dataIfNil, response, error) in
-                
-                // Hide HUD once the network request comes back (must be done on main UI thread)
-                MBProgressHUD.hideHUDForView(self.view, animated: true)
-                if let data = dataIfNil {
-                    //data is coming back as responseDictionary
-                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-                        data, options:[]) as? NSDictionary {
-                            //print("response: \(responseDictionary)")
-                            
-                            //use self when inside an enclosure!
-                            self.movies = responseDictionary["results"] as! [NSDictionary]
-                            self.tableView.reloadData()
-                    }
-                }
-                
-                // ... Remainder of response handling code ...
-                
-        });
-        task.resume()
     }
     
     // Makes a network request to get updated data
@@ -165,7 +198,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     func refreshControlAction(refreshControl: UIRefreshControl) {
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
         let myRequest = NSURLRequest(
             URL: url!,
             cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
@@ -224,6 +257,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
+    
+    
+    
+    
+    
     
 
 }
